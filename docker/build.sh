d15e62e8213ecc384e263515458c373ae0e5fe7c
NTheme <nikitin.artem.a@gmail.com>
2025-09-21 16:03:08 +0300

    Universal commits presents...

--- docker/build.sh
+++ docker/build.sh
@@ -14,7 +14,7 @@ adduser -c 'ejudge compiler' -d /home/judges/compile -M -s /sbin/nologin ejcompi
 RELEASE='RELEASE=1'
 
 rm -rf .git
-./configure --prefix=/opt/ejudge --enable-charset=utf-8 --with-httpd-cgi-bin-dir=/var/www/cgi-bin --with-httpd-htdocs-dir=/var/www/html --enable-ajax --enable-local-dir=/var/lib/ejudge --enable-hidden-server-bins --with-primary-user=ejudge --with-exec-user=ejexec --with-compile-user=ejcompile --enable-compile-spool-dir --enable-run-spool-dir --enable-contests-status-dir && ulimit -n 65536 && make ${RELEASE} && make ${RELEASE} install && /opt/ejudge/bin/ejudge-suid-setup && /opt/ejudge/bin/ejudge-upgrade-web
+./configure --prefix=/opt/ejudge --enable-charset=utf-8 --with-httpd-cgi-bin-dir=/var/www/cgi-bin --with-httpd-htdocs-dir=/var/www/html --enable-ajax --enable-local-dir=/var/lib/ejudge --enable-hidden-server-bins --with-primary-user=ejudge --with-exec-user=ejexec --with-compile-user=ejcompile --enable-compile-spool-dir --enable-run-spool-dir && ulimit -n 65536 && make ${RELEASE} && make ${RELEASE} install && /opt/ejudge/bin/ejudge-suid-setup && /opt/ejudge/bin/ejudge-upgrade-web
 
 > ejudge-install.sh
 chmod 755 ejudge-install.sh
@@ -25,7 +25,9 @@ cp docker/httpd.conf /etc/httpd/conf
 
 wget https://ejudge.ru/download/ejudge-container-fedora-36.tbz
 mv ejudge-container-fedora-36.tbz /opt/ejudge/share/ejudge
-( cd /opt/ejudge/share/ejudge ; tar xf ejudge-container-fedora-36.tbz )
+( cd /opt/ejudge/share/ejudge ; tar --exclude='dev/*' -xf ejudge-container-fedora-36.tbz )
 rm /opt/ejudge/share/ejudge/ejudge-container-fedora-36.tbz
 cp -p /etc/passwd /opt/ejudge/share/ejudge/container/etc
 cp -p /etc/group /opt/ejudge/share/ejudge/container/etc
+
+mkdir /home/judges; cp -r /opt/ejudge/bin /home/judges/bin
